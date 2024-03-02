d4f5594800bfc28ee7a582525f7efb54aa428b4d
Kevin <laig.kevin@gmail.com>
2024-03-02 11:42:13 +0200

    Initial commit

---
+++ src/test/java/sda/jre28/travelagency/TravelagencyApplicationTests.java
@@ -0,0 +1,13 @@
+package sda.jre28.travelagency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelagencyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
