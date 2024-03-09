8d0232ce0b806b46aa8297bdadba58420f5593b5
Kevin <laig.kevin@gmail.com>
2024-03-09 13:53:03 +0200

    Started creating tests for AdminService, PurchaseDataService and TourService

--- src/main/java/sda/jre28/travelagency/exceptions/NoAvailableSeatsException.java
+++ src/main/java/sda/jre28/travelagency/exceptions/NoAvailableSeatsException.java
@@ -1,10 +1,22 @@
 package sda.jre28.travelagency.exceptions;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class NoAvailableSeatsException extends Exception{
     private String message;
 
     public NoAvailableSeatsException(String message) {
         super(message);
         this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return "NoAvailableSeatsException{" +
+                "message='" + message + '\'' +
+                '}';
     }
 }
