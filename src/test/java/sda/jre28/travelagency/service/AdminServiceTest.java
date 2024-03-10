fa5eeffa857833db1ad601d455184ae5349481b7
JaanKrux <jaankrukovski@gmail.com>
2024-03-10 11:02:47 +0200

    test wrote for admin services

--- src/test/java/sda/jre28/travelagency/service/AdminServiceTest.java
+++ src/test/java/sda/jre28/travelagency/service/AdminServiceTest.java
@@ -5,13 +5,19 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import sda.jre28.travelagency.model.CityType;
+import sda.jre28.travelagency.model.PurchaseData;
 import sda.jre28.travelagency.model.Tour;
 import sda.jre28.travelagency.repository.AdminRepository;
 import sda.jre28.travelagency.repository.PurchaseDataRepository;
 import sda.jre28.travelagency.repository.TourRepository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
+
 
 
 @ExtendWith(MockitoExtension.class)
@@ -50,15 +56,66 @@ public class AdminServiceTest {
     @Test
     public void testUpdateTour_returnSuccessfully() {
 
+        double adultPrice = 100 ;
+
+        Tour tour = new Tour();
+        tour.setAdultPrice(adultPrice );
+        when(tourRepository.save(tour)).thenReturn(tour);
+
+        Tour tourResult = adminService.createTour(tour);
+
+        assertEquals(tourResult.getAdultPrice(), adultPrice);
+
     }
 
     @Test
     public void testDeleteTour_returnSuccessfully() {
 
+        CityType destination = CityType.ROME ;
+
+        Tour tour = new Tour();
+        tour.setDestination (destination);
+        when(tourRepository.save(tour)).thenReturn(tour);
+
+        Tour tourResult = adminService.createTour(tour);
+
+        assertEquals(tourResult.getDestination(), destination);
+
+
     }
 
     @Test
     public void testFindAllPurchaseDatas_returnSuccessfully() {
+
+        Long purchaseDataId = 1L;
+
+        int numberOfAdults = 5 ;
+        int numberOfChildren = 4 ;
+        Long tourId = 1L ;
+        Long userId = 1L ;
+        boolean isPurchased = true ;
+
+        PurchaseData purchaseData = new PurchaseData();
+
+        purchaseData.setPurchased(isPurchased);
+        purchaseData.setNumberOfAdults(numberOfAdults);
+        purchaseData.setNumberOfChildren(numberOfChildren);
+        purchaseData.setTourId(tourId);
+        purchaseData.setId(purchaseDataId);
+        purchaseData.setUserId(userId);
+
+        when(purchaseDataRepository.findAll()).thenReturn(List.of(purchaseData));
+
+        List<PurchaseData> listResult = adminService.findAllPurchaseDatas();
+
+        assertEquals(listResult.get(0).getId(),purchaseDataId) ;
+        assertEquals(listResult.get(0).getNumberOfAdults(),numberOfAdults)  ;
+        assertEquals(listResult.get(0).getTourId(),tourId)  ;
+        assertEquals(listResult.get(0).getUserId(),userId)  ;
+        assertEquals(listResult.get(0).getNumberOfChildren(),numberOfChildren)  ;
+        assertEquals(listResult.get(0).isPurchased(),isPurchased)  ;
+
+
 
     }
 
